259b92fb2eb97936b3ef23bf458a5e4363a84576
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-16 17:21:14 -0300

    project init

--- server/route/blogRoute.js
+++ server/route/blogRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const basePath = 'blog-api/v1'
+const basePath = '/blog-api/v1'
 
 router.get(`${basePath}/posts`, async function (req, res) {});
 router.get(`${basePath}/posts:id`, async function (req, res) {});
