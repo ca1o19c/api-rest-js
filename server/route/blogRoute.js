d9fc734214fec804d99fa258d0265bccf3e14243
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-16 20:15:41 -0300

    project init

--- server/route/blogRoute.js
+++ server/route/blogRoute.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/posts', async function (req, res) {});
+router.get('/posts', async function (req, res) {
+  res.end();
+});
+
 router.get('/posts/:id', async function (req, res) {});
 router.post('/posts', async function (req, res) {});
 router.put('/posts/:id', async function (req, res) {});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.path('/blog-api/v1');
+app.use('/blog-api/v1', require('./route/blogRoute'));
 app.listen(3000);
