9aa4a5ebf9708ab714f922b87a71844ad41a31ab
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-21 23:20:21 -0300

    feat: created the flow of creating a blog post

--- server/route/blogRoute.js
+++ server/route/blogRoute.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const blogService = require('../service/blogService');
 
 router.get('/posts', async function (req, res) {
-  const posts = await blogService.getPosts();
+  const posts = await blogService.findAll();
   res.json(posts);
 });
 
-router.get('/posts/:id', async function (req, res) {});
-router.post('/posts', async function (req, res) {});
-router.put('/posts/:id', async function (req, res) {});
-router.delete('/posts/:id', async function (req, res) {});
+router.post('/posts', async function (req, res) {
+  const payload = req.body;
+
+  const save = await blogService.save(payload);
+  res.json(save);
+});
 
 module.exports = router;
--- server/test/blog.test.js
+++ server/test/blog.test.js
@@ -1,18 +1,48 @@
 const axios = require('axios');
+const blogService = require('../service/blogService');
+const crypto = require('crypto');
+
+const generate = function () {
+  return crypto.randomBytes(20).toString('hex')
+}
+
 const basePath = 'http://localhost:3000/blog-api/v1';
 
+const request = function (url, method, data) {
+
+  return axios({ url, method, data })
+}
+
 test('Should get posts', async function () {
-  const response = await axios({
-    url: `${basePath}/posts`,
-    method: 'get'
-  });
+
+  const post1 = await blogService.save({ title: generate(), content: generate() })
+  const post2 = await blogService.save({ title: generate(), content: generate() })
+  const post3 = await blogService.save({ title: generate(), content: generate() })
+
+  const response = await request(`${basePath}/posts`, 'get');
 
   const posts = response.data;
 
   expect(posts).toHaveLength(3);
 
-  const [firstPost] = posts;
+  await blogService.delete(post1.id);
+  await blogService.delete(post2.id);
+  await blogService.delete(post3.id);
+});
 
-  expect(firstPost.id).toBe(1);
-  expect(firstPost.title).toBe('REST API: Métodos');
+test('Should save posts', async function () {
+
+  const payload = { title: generate(), content: generate() };
+
+  const response = await request(`${basePath}/posts`, 'post', payload);
+
+  const post = response.data;
+
+  expect(post).not.toBeNull()
+  expect(post.title).toBe(payload.title)
+  expect(post.content).toBe(payload.content)
+
+  console.log(post.id);
+
+  await blogService.delete(post.id);
 });
