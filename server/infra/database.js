463e02aecbd980c5a5493e17ecc136fbea1618f2
Caio Antonio <caio.antonio.c@outlook.com>
2022-01-21 21:58:39 -0300

    chore: created environmentes variables

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,12 @@
 const pgp = require('pg-promise')();
+require('dotenv/config');
+
 const db = pgp({
-  user: 'root',
-  password: 'root',
-  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_USER,
+  host: process.env.DB_HOST,
   port: 5432,
-  database: 'postgres'
+  database: process.env.DB_NAME
 });
 
 module.exports = db;
